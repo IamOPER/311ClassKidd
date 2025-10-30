d819d8820f8a14ac828e544c1c30afa9098b8c40
adas5 <adas5@linux6.gl.umbc.edu>
2025-10-30 12:21:22 -0400

    First commenit

--- hello.c
+++ hello.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
 int main(){
-  printf( "Hello teach. My name is Abhinna and I'm in your CMPE311 class.")
+  printf( "Hello teach. My name is Abhinna and I'm in your CMPE311 class.\n");
+return 0;
 }
